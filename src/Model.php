1675ba65d205f49b6a01cd94d2c3451e2b4c4575
Colber-Ubuntu <colber@simplestart.cn>
2021-06-03 19:59:45 +0800

    添加关联OR查询

--- src/Model.php
+++ src/Model.php
@@ -173,47 +173,67 @@ class Model extends \think\Model
             return $this->where($input);
         } else if (count($input) > 0) {
             // 数据字典
-            $_table = $this->name ?: $this->getTable();
-            $_tableFields = Cache::get($_table.'_fields');
-            if(empty($_tableFields)){
-                $_tableFields = $this->getTableFields();
-                Cache::set($_table.'_fields', $_tableFields);
+            $table = $this->getTable();  // 主表
+            $tableFields = Cache::get($table.'_fields');
+            if(empty($tableFields)){
+                $tableFields = $this->getTableFields();
+                Cache::set($table.'_fields', $tableFields);
             }
             $query    = null; // 查询对象(Query)
-            $table    = ''; // 查询表格(主表格)
-            $relation = array(); // 关联模型及条件
+            // $relation = array(); // 关联模型及条件
             foreach ($input as $key => $value) {
                 // 参数过滤
-                if(stripos($key, '|') === false && stripos($key, '.') === false){
-                    if(!in_array($key, $_tableFields)){
-                        unset($input[$key]);
-                    }
+                if(stripos($key, '|') === false && stripos($key, '.') === false && !in_array($key, $tableFields)){
+                    unset($input[$key]);
                 }
                 // 关联查询
-                if (stripos($key, '.') !== false) {
+                if(stripos($key, '|') !== false && stripos($key, '.') !== false) {
+                    // 关联 OR 查询
+                    $orQuery = explode('|', $key);
+                    $relation = array();
+                    foreach ($orQuery as $orField) {
+                        if (stripos($orField, '.') !== false) {
+                            list($model, $field) = explode('.', $orField);
+                            !isset($relation[$model]) ? $relation[$model] = [] : '';
+                            $relation[$model][$field]                     = $value;
+                        }else{
+                            !isset($relation['this']) ? $relation['this'] = [] : '';
+                            $relation['this'][$orField]                   = $value;
+                        }
+                    }
+                    foreach ($relation as $model => $condition) {
+                        if($model === 'this'){
+                            if (is_null($query)) {
+                                $query = $this->parseFilter($this, $condition, $table, "OR");
+                            } else {
+                                $query = $this->parseFilter($query, $condition, $table, "OR");
+                            }
+                        }else{
+                            $relateTable = ucfirst($model);
+                            if (is_null($query)) {
+                                $query = $this->hasWhere($model, $this->parseFilter($this, $condition, $relateTable, 'OR'));
+                            } else {
+                                $query = $query->hasWhere($model, $this->parseFilter($query, $condition, $relateTable, 'OR'));
+                            }
+                        }
+                    }
+                    unset($input[$key]);
+                }else if (stripos($key, '.') !== false) {
+                    // 关联 AND 查询
                     list($model, $field) = explode('.', $key);
-                    if (!empty($value) || is_numeric($value)) {
-                        !isset($relation[$model]) ? $relation[$model] = [] : '';
-                        $relation[$model][$field]                     = $value;
+                    $relateTable = ucfirst($model);
+                    if (is_null($query)) {
+                        $query = $this->hasWhere($model, $this->parseFilter($this, [$field => $value], $relateTable));
+                    } else {
+                        $query = $query->hasWhere($model, $this->parseFilter($query, [$field => $value], $relateTable));
                     }
                     unset($input[$key]);
                 }
             }
 
-            // 关联查询
-            if (count($relation) > 0) {
-                $table = $this->getTable();
-                foreach ($relation as $model => $condition) {
-                    if (is_null($query)) {
-                        $query = $this->hasWhere($model, $this->parseFilter($this, $condition));
-                    } else {
-                        $query = $query->hasWhere($model, $this->parseFilter($query, $condition));
-                    }
-                }
-            }
             // 单表查询
             if (is_null($query)) {
-                $query = $this->parseFilter($this, $input, $table);
+                $query = $this->parseFilter($this, $input,  $table);
             } else {
                 $query = $this->parseFilter($query, $input, $table);
             }
@@ -226,30 +246,54 @@ class Model extends \think\Model
      * @param  [type] $query     [description]
      * @param  array  $condition [description]
      * @param  string $table     [description]
+     *  @param string $logic     查询逻辑 AND OR
      * @return [type]            [description]
      */
-    private function parseFilter($query, array $condition = [], $table = '')
+    private function parseFilter($query, array $condition = [], $table = '', $logic = 'AND')
     {
         
         $operator = ['=', '<>', '>', '>=', '<', '<=', 'like', 'not like', 'in', 'not in', 'between', 'not between', 'null', 'not null', 'exists', 'not exists', 'regexp', 'not regexp'];
         if (!empty($table) && stripos($table, '.') === false) {
             $table .= '.';
         }
+
         foreach ($condition as $key => $value) {
             if (empty($value) && !is_numeric($value)) {
                 continue;
             }
             if (is_array($value)) {
                 if (count($value) > 1 && in_array(strtolower($value[0]), $operator)) {
-                    $query = $query->where($table . $key, $value[0], $value[1]);
+                    $query = $logic === 'AND' ? $query->where($table . $key, $value[0], $value[1]) : $query->whereOr($table . $key, $value[0], $value[1]);
                 } else {
-                    $query = $query->where($table . $key, 'in', $value);
+                    $query = $logic === 'AND' ? $query->where($table . $key, 'in', $value) : $query->whereOr($table . $key, 'in', $value);
                 }
             } else {
-                $query = $query->where($table . $key, $value);
+                $query = $logic === 'AND' ? $query->where($table . $key, $value) : $query->whereOr($table . $key, $value);
             }
         }
         return $query;
+    }
+
+    /**
+    * 下划线转驼峰
+    * 思路:
+    * step1.原字符串转小写,原字符串中的分隔符用空格替换,在字符串开头加上分隔符
+    * step2.将字符串中每个单词的首字母转换为大写,再去空格,去字符串首部附加的分隔符.
+    */
+    private function toCamelCase($uncamelized_words, $separator='_')
+    {
+        $uncamelized_words = $separator. str_replace($separator, " ", strtolower($uncamelized_words));
+        return ltrim(str_replace(" ", "", ucwords($uncamelized_words)), $separator );
+    }
+
+    /**
+    * 驼峰命名转下划线命名
+    * 思路:
+    * 小写和大写紧挨一起的地方,加上分隔符,然后全部转小写
+    */
+    private function toUnderScore($camelCaps, $separator='_')
+    {
+        return strtolower(preg_replace('/([a-z])([A-Z])/', "$1" . $separator . "$2", $camelCaps));
     }
 
 }
